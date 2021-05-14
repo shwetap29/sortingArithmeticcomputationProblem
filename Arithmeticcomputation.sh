fec15384068eb5481b97ec6e3b09a20c8f2e1b24
shwetap29 <shwetaprasad48@gmail.com>
2021-05-14 15:57:59 +0530

    added arithmetic operation1 in Arithmeticcomputation.sh

--- Arithmeticcomputation.sh
+++ Arithmeticcomputation.sh
@@ -3,4 +3,5 @@ echo "Welcome to arithmetic computation problem"
 read -p "Enter number: " a
 read -p "Enter number: " b
 read -p "Enter number :" c
-
+operation1=`echo "scale=4; $a + $b * $c"   #|bc -1`
+echo "a+b*c = $operation1"
