9e5da919c4e9ee943f52f864e019bae235f28c4f
shwetap29 <shwetaprasad48@gmail.com>
2021-05-14 16:19:18 +0530

    added second arithmetic operation in Arithmeticcomputation.sh

--- Arithmeticcomputation.sh
+++ Arithmeticcomputation.sh
@@ -1,7 +1,12 @@
 #printing message
 echo "Welcome to arithmetic computation problem"
+#taking input from the user
 read -p "Enter number: " a
 read -p "Enter number: " b
 read -p "Enter number :" c
+#performing arithmetic operation
 operation1=`echo "scale=4; $a + $b * $c"   #|bc -1`
+operation2=`echo "scale=4; $a * $b + $c"   #|bc -1`
+
 echo "a+b*c = $operation1"
+echo "a*b+c = #operation2"
