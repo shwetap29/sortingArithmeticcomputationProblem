912f74e03edb3323534ce01cf3708918f4967e71
shwetap29 <shwetaprasad48@gmail.com>
2021-05-14 16:41:16 +0530

    added Fourth arithmetic operation in Arithmeticcomputation.sh

--- Arithmeticcomputation.sh
+++ Arithmeticcomputation.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -x
+
 #printing message
 echo "Welcome to arithmetic computation problem"
 #taking input from the user
@@ -8,6 +10,10 @@ read -p "Enter number :" c
 operation1= echo "scale=4; $a + $b * $c"  #|bc -1`
 operation2= echo "scale=4; $a * $b + $c"  #|bc -1`
 operation3= echo "scale=4; $c + $a / $b"  #|bc -1`
+operation4=`echo "scale=4; $a % $b + $c"  #|bc -l`
+
+#printing output and message
 echo "a+b*c = $operation1"
 echo "a*b+c = $operation2"
 echo "c+a/b = $operation3"
+echo "a%b+c = $operation4"
